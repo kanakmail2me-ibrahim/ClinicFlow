319ae6e1518d745c9903ef75859c40e306f1ba6d
kanakmail2me-ibrahim <kanakmail2me@gmail.com>
2026-04-05 18:08:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def admin_dashboard():
     conn = get_db_connection()
     cur = conn.cursor()
     # Saare appointments ko naye se purane (DESC) order mein mangwao
-    cur.execute("SELECT id, patient_name, phone, appt_date, message FROM appointments ORDER BY id DESC")
+    cur.execute("SELECT id, patient_name, phone, appt_date, message, status, age FROM appointments ORDER BY id DESC")
     appointments = cur.fetchall()
     cur.close()
     conn.close()
